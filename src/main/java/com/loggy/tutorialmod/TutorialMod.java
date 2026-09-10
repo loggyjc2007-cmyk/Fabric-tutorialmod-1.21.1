2733b8500245ab4ed4b4cb063b90e78a35834c89
Voidwalker_07 <loggyjc2007@gmai9l.com>
2026-09-10 16:49:02 +0100

    added hello among other things

---
+++ src/main/java/com/loggy/tutorialmod/TutorialMod.java
@@ -0,0 +1,20 @@
+package com.loggy.tutorialmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.minecraft.util.Identifier;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+//hello
+public class TutorialMod implements ModInitializer {
+	public static final String MOD_ID = "tutorialmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {}
+
+	public static Identifier id(String path) {
+		return Identifier.of(MOD_ID, path);
+	}
+}
